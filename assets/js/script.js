3f25d276901a7812d3ecefe05f9cc6d95764cf73
idrismikayil <idrisrm@code.edu.az>
2021-12-04 01:35:44 +0400

    html script src edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,4 +16,28 @@ function ReverseString(str) {
   
 console.log(ReverseString("code academy"))
 
-//2ci taskı birçox üsul ilə yoxlamağıma baxmayaraq həll edə bilmədim
+class CustomMatch {
+    constructor(numberr) {
+        this.numberr = numberr;
+    }
+    plus = function (plus) {
+        this.numberr += plus;
+        return this;
+    }
+    minus = function (minus) {
+        this.numberr-= minus;
+        return this;
+    }
+    multiply = function (multiply) {
+        this.numberr *= multiply;
+        return this;
+    }
+    divide = function (divide) {
+        this.numberr /= divide;
+        return this.numberr;
+    }
+}
+
+var result = new CustomMatch(50).plus(6).minus(30).multiply(3).divide(2);
+
+console.log(result);
